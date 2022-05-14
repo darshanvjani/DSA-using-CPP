7491c8ab64366c0b43aec9383e7de7effae64329
darshanvjani <35656144+darshanvjani@users.noreply.github.com>
2022-05-14 11:22:26 +0530

    Add files via upload

---
+++ Arrays/Sorting Algorithm/Insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n,number;
+    cout<<"Enter The Size of Array"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter number to story in array"<<endl;
+
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=1;i<n;i++){
+        int current = arr[i];
+        int j = i - 1;
+
+        while(arr[j]>current && j>=0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
